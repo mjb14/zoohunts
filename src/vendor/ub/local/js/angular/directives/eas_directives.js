98da0076485c78b5bb278fc12b8bdfcfc75b6d0f
Mike Brennan <mjb14@buffalo.edu>
2013-10-05 10:36:44 -0400

    remove files

--- src/vendor/ub/local/js/angular/directives/eas_directives.js
+++
@@ -1,8 +0,0 @@
-/*
-    eas_directives.js
-    
-    This module exists to pull in all directives that should be common to all apps, which could
-    include eas created directives or vendor created directives.
-*/
-
-var directivesModule = angular.module('EASDirectives', ['EASTitleBarModule', 'EASPreventClickModule', 'EASTypeahead', 'EASKeyData', 'easgrid']);
--- src/vendor/ub/local/js/common/eas_variables.js
+++
@@ -1,7 +0,0 @@
-var EAS = EAS || {};
-
-EAS.globalMessages = EAS.globalMessages || {}
-
-EAS.globalMessages.leavingPageText = 'If you navigate away from this page without saving, you will lose all changes made since your last save.  Click Cancel to stay on the current page.';
-
-EAS.globalMessages.onlyShowingFirstXRecords = 'Only showing the first :var0 records.';

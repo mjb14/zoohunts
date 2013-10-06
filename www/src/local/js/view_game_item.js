bb486f4779638993acea3908990bd9cf6f0590e0
Mike Brennan <mjb14@buffalo.edu>
2013-10-06 01:01:21 -0400

    updated

--- www/src/local/js/view_game_item.js
+++ www/src/local/js/view_game_item.js
@@ -79,6 +79,44 @@ viewGameItemModule.controller('ViewGameItemController', [
         $location.path('/view-game-items/' + game_instance.game_instance_id);
     }
     
+    $scope.scan_result;
+    
+    $scope.checkAnswer = function() {
+         try {
+            var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+            scanner.scan( function (result) { 
+
+/*
+                alert("We got a barcode\n" + 
+                "Result: " + result.text + "\n" + 
+                "Format: " + result.format + "\n" + 
+                "Cancelled: " + result.cancelled);  
+
+               console.log("Scanner result: \n" +
+                    "text: " + result.text + "\n" +
+                    "format: " + result.format + "\n" +
+                    "cancelled: " + result.cancelled + "\n");
+  */
+  
+            $scope.scan_result = result.text;
+                
+                /*
+                if (args.format == "QR_CODE") {
+                    window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+                }
+                */
+
+            }, function (error) { 
+                console.log("Scanning failed: ", error); 
+            } );
+        } catch (ex) {
+            console.log(ex.message);
+        }
+        
+        
+    }
+    
 
 }]);
 
